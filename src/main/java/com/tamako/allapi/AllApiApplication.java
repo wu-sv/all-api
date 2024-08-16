14c15cad2cf72cc3b42e9bd15397fac6ecfd90d3
Tamako <1253594122@qq.com>
2024-08-16 10:12:54 +0800

    123

---
+++ src/main/java/com/tamako/allapi/AllApiApplication.java
@@ -0,0 +1,13 @@
+package com.tamako.allapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamako/allapi/AllApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamako.allapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
