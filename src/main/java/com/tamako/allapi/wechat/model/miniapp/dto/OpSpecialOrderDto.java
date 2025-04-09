86cf221b589086d9e34a07fe025a2eafe9ae6f9e
Tamako <1253594122@qq.com>
2025-04-09 10:35:32 +0800

    V1.2.10 1.修复了微信特殊发货报备的bug 2.升级所有的依赖

--- src/main/java/com/tamako/allapi/wechat/model/miniapp/dto/OpSpecialOrderDto.java
+++ src/main/java/com/tamako/allapi/wechat/model/miniapp/dto/OpSpecialOrderDto.java
@@ -1,6 +1,7 @@
 package com.tamako.allapi.wechat.model.miniapp.dto;
 
 
+import cn.hutool.core.annotation.Alias;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.jetbrains.annotations.NotNull;
@@ -18,6 +19,7 @@ public class OpSpecialOrderDto {
      * 需要特殊发货报备的订单号，可传入微信支付单号或商户单号
      */
     @NotNull
+    @Alias("order_id")
     private String orderId;
     /**
      * 特殊发货报备类型，1为预售商品订单，2为测试订单
@@ -27,5 +29,6 @@ public class OpSpecialOrderDto {
     /**
      * 预计发货时间的unix时间戳，type为1时必填，type为2可省略
      */
+    @Alias("delay_to")
     private Integer delayTo;
 }
